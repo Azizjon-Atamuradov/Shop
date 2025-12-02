302f3cb646485d5263c3cb1d35e9d527c03b5938
Aziz <azizatamuradov1994@gmail.com>
2025-12-02 09:33:21 -0500

    Navbar component created

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png"
@@ -8,10 +8,10 @@ const Navbar = () => {
     <div className="navbar">
       <div className="nav-logo">
               <img src={logo} alt="" />
-              <p>Shop</p>
+              <p>Aziz Shop</p>
           </div>
           <ul className="nav-menu">
-              <li>Shop</li>
+              <li id="a">Shop <hr /></li>
               <li>Men</li>
               <li>Women</li>
               <li>Kids</li>
