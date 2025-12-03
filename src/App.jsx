7f92c6a5a3c1d267095cacc97d82a7dd57a66eeb
Aziz <azizatamuradov1994@gmail.com>
2025-12-03 08:54:22 -0500

    navbar component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Shop from './Pages/Shop'
+
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+       <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+      </Routes>
+      
+      
+      </BrowserRouter>
+     
     </div>
   )
 }
